870d88ecc2b756bdf4292c2bfecb051f4b1286e0
WatcharakorP <98681390+WatcharakorP@users.noreply.github.com>
2023-01-15 22:06:48 +0700

    Add files via upload

---
+++ Youtube_SendData_toHive.py
@@ -0,0 +1,59 @@
+import time
+import pytchat
+import json
+import mysql.connector
+from transformers import pipeline
+from time import sleep
+import paho.mqtt.client as paho
+from paho import mqtt
+from worldometer import Worldometer
+
+
+
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("CONNACK received with code %s." % rc)
+
+def on_publish(client, userdata, mid, properties=None):
+    print("mid: " + str(mid))
+
+
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+client.on_connect = on_connect
+
+
+client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+
+client.username_pw_set("Mekalone", "Watcharakorn007")
+
+client.connect("e519cd77d46c464383728f1639fa2c4a.s1.eu.hivemq.cloud", 8883)
+
+client.on_subscribe = on_subscribe
+client.on_message = on_message  
+client.on_publish = on_publish  
+
+
+
+
+client.loop_start()
+
+chat = pytchat.create(video_id="EBcvHK2Moys")
+
+while chat.is_alive():
+        for c in chat.get().sync_items():
+            print(f'{c.datetime} {c.author.name} {c.message}')
+            print(c.message)
+            client.publish("youtubelive", payload=json.dumps(c.message, ensure_ascii=False).encode('utf8'), qos=1)
+            #client.publish("youtubelive", payload=json.dumps(c.message), qos=1)
+            #client.publish("youtubelive", payload=c.message, qos=1)
+
+
+   
+client.loop_stop()
